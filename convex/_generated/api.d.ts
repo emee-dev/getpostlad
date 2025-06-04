2f51d36d11a5ae0becb59104cfaaec4af471d01e
Emmanuel Ajike <117521496+emee-dev@users.noreply.github.com>
2025-06-04 12:23:32 +0100

    Added dataModel.d.ts

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,38 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+import type * as environments from "../environments.js";
+import type * as workspaces from "../workspaces.js";
+
+/**
+ * A utility for referencing Convex functions in your app's API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+declare const fullApi: ApiFromModules<{
+  environments: typeof environments;
+  workspaces: typeof workspaces;
+}>;
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
