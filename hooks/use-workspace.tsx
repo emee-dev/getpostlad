8d2273d46f5fe41c6655a906d86da443e4580912
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2025-06-08 18:16:37 +0100

    Added use-workspace.tsx

---
+++ hooks/use-workspace.tsx
@@ -0,0 +1,49 @@
+import { Id } from "@/convex/_generated/dataModel";
+import { create } from "zustand";
+
+export type Workspace = {
+  _id: Id<"workspaces">;
+  _creationTime: number;
+  name: string;
+  userId: string;
+};
+
+export type Variable = {
+  id: string;
+  key: string;
+  value: string;
+};
+
+export type Environment = {
+  _id: Id<"environments">;
+  _creationTime: number;
+  name: string;
+  workspaceId: Id<"workspaces">;
+  variables: Variable[];
+};
+
+interface WorkspaceState {
+  workspaces: Workspace[];
+  selectedWorkspace: Workspace | null;
+  setSelectedWorkspace: (ws: Workspace) => void;
+  setWorkspaces: (ws: Workspace[]) => void;
+
+  // Environments
+  environments: Environment[];
+  selectedEnvironment: Environment | null;
+  setSelectedEnvironment: (ws: Environment) => void;
+  setEnvironments: (ws: Environment[]) => void;
+}
+
+export const useWorkspace = create<WorkspaceState>((set, get) => ({
+  workspaces: [],
+  environments: [],
+  selectedWorkspace: null,
+  selectedEnvironment: null,
+  expandedFolders: new Set(),
+
+  setSelectedWorkspace: (ws) => set({ selectedWorkspace: ws }),
+  setWorkspaces: (ws) => set({ workspaces: ws }),
+  setSelectedEnvironment: (env) => set({ selectedEnvironment: env }),
+  setEnvironments: (env) => set({ environments: env }),
+}));
