64651568138088e50b752b51a5f7c67d8b89578a
Ajike Emmanuel Jonah <emmanuelajike2000@gmail.com>
2025-06-08 17:51:21 +0100

    Update theme-toggle.tsx

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -11,28 +7,37 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Check, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
+          variant="ghost"
+          size="icon"
+          className="size-7 hover:bg-muted-foreground/20 hover:dark:bg-muted-foreground/15"
+        >
+          <Sun className="h-4 transition-all scale-100 rotate-0 dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-4 transition-all scale-0 rotate-90 dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent side="right" align="start">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          {theme === "light" && <Check className="w-4 mr-2" />}{" "}
+          {theme !== "light" && <div className="w-4 mr-2" />} Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          {theme === "dark" && <Check className="w-4 mr-2" />}
+          {theme !== "dark" && <div className="w-4 mr-2" />} Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          {theme === "system" && <Check className="w-4 mr-2" />}
+          {theme !== "system" && <div className="w-4 mr-2" />} System
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
