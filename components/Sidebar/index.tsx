0b18eeda2d85150558d0da4e0c782bf464151969
Emmanuel Ajike <117521496+emee-dev@users.noreply.github.com>
2025-06-11 13:30:35 +0100

    Updated index.tsx

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -53,8 +53,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         className={cn({
           hidden: open,
           block: !open,
-        })}
-      />
+        })}>
+        <SidebarGroup>
+          <SidebarGroupLabel className="flex items-center">
+            <span className="font-geist">Collection</span>
+            <div className="ml-auto"></div>
+          </SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <FileExplorer />
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
       <SidebarRail />
     </Sidebar>
   );
